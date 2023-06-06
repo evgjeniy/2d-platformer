fbd1f2ddb5e39e424ab0841b1a525ddead12006d
evgjeniy <dashkevich2016@mail.ru>
2023-06-06 19:18:08 +0300

    fix yandex cloud save system

--- Assets/Scripts/Utils/YandexCloudSaveData.cs
+++ Assets/Scripts/Utils/YandexCloudSaveData.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections;
+using System.Collections.Generic;
 using Agava.YandexGames;
 using Newtonsoft.Json;
 using UnityEngine;
@@ -9,50 +9,67 @@ namespace Utils
     public static class YandexCloudSaveData
     {
         private static Dictionary<string, string> _saveDictionary = null;
+        private static MonoBehaviour _context;
 
         public static void Save(string key, string value)
         {
-            CheckSaveDictionary();
+            CheckContext();
+            _context.StartCoroutine(CheckSaveDictionary());
             
             _saveDictionary[key] = value;
 
-            var jsonString = JsonConvert.SerializeObject(_saveDictionary);
-            
-#if !UNITY_WEBGL || UNITY_EDITOR
-            var jsonSaveDictionary = JsonConvert.SerializeObject(_saveDictionary);
-            PlayerPrefs.SetString("SaveDataDictionary", jsonSaveDictionary);
-            PlayerPrefs.Save();
-#else
-            PlayerAccount.SetCloudSaveData(jsonString);
-#endif
-
-            Debug.Log(jsonString + "\n\n" + _saveDictionary.Aggregate("{", (current, pair) => current + $"\"{pair.Key}\" : \"{pair.Value}\"") + "}");
+            _context.StartCoroutine(SaveJsonToCloud());
         }
 
         public static string Get(string key, string defaultValue = null)
         {
-            CheckSaveDictionary();
+            CheckContext();
+            _context.StartCoroutine(CheckSaveDictionary());
 
             return _saveDictionary.TryGetValue(key, out var value) ? value : defaultValue;
         }
 
         public static void Delete(string key) => _saveDictionary.Remove(key);
-
-        private static void CheckSaveDictionary()
+        
+        private static void CheckContext()
         {
-            if (_saveDictionary != null) return;
+            if (_context == null)
+                _context = Object.FindObjectOfType<BackgroundAudio>();
+        }
+        
+        private static IEnumerator SaveJsonToCloud()
+        {
+            var jsonString = JsonConvert.SerializeObject(_saveDictionary);
+
+#if !UNITY_WEBGL || UNITY_EDITOR
+            PlayerPrefs.SetString("SaveDataDictionary", jsonString);
+            PlayerPrefs.Save();
+#else
+            if (!YandexGamesSdk.IsInitialized) 
+                yield return YandexGamesSdk.Initialize(); 
+            
+            PlayerAccount.SetCloudSaveData(jsonString);
+#endif
+            yield break;
+        }
+        
+        private static IEnumerator CheckSaveDictionary()
+        {
+            if (_saveDictionary != null) yield break;
             
 #if !UNITY_WEBGL || UNITY_EDITOR
             var jsonSaveDictionary = PlayerPrefs.GetString("SaveDataDictionary", "{}");
             _saveDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonSaveDictionary);
 #else
+            if (!YandexGamesSdk.IsInitialized)
+                yield return YandexGamesSdk.Initialize();
+
             PlayerAccount.GetCloudSaveData(jsonCloudData =>
             {
                 _saveDictionary = jsonCloudData == default 
                     ? new Dictionary<string, string>() 
                     : JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonCloudData);
             }, onErrorCallback: _ => _saveDictionary = new Dictionary<string, string>());
-
 #endif
         }
     }
